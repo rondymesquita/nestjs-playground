69812443d5b386978a6f6ab5f277c708f1a477c0
rondymesquita <rondinellimesquitas@gmail.com>
2021-07-17 13:37:35 -0300

    add middleware on standalone app but without success

--- standalone-app/src/app.module.ts
+++ standalone-app/src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserAgentMiddleware } from './app.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UserAgentMiddleware).forRoutes(AppController);
+  }
+}
--- standalone-app/src/main.ts
+++ standalone-app/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const controller = <AppController>app.select(AppModule).get(AppController);
+  console.log(controller.getHello());
+  app.listen(3000);
+
+  const app2 = await NestFactory.createApplicationContext(AppModule);
+  const controller2 = <AppController>app2.select(AppModule).get(AppController);
+  console.log(controller2.getHello());
 }
 bootstrap();
